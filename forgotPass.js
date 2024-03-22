9687cabe0a15d4c235af0e48cdedb07ad0e6c7e7
Grace4501 <g.andrews954@mybvc.ca>
2024-03-22 17:38:16 -0600

    finished create account

--- forgotPass.js
+++ forgotPass.js
@@ -5,9 +5,11 @@
         event.preventDefault();
 
         const usernameInput = document.getElementById("username").value;
-        const user = users.find(user => user.username === usernameInput);
+        const UserOwner = ownerUsers.find(user => user.username === usernameInput);
+        const UserWorker = workerUsers.find(user => user.username === usernameInput);
 
-        if(user) {
+        if(UserOwner || UserWorker) {
+            const user = UserOwner || UserWorker;
             document.getElementById("passwordResult").innerHTML = `Password for ${usernameInput}: ${user.password}`;
         } else {
             document.getElementById("passwordResult").innerHTML = "User not found. Please try again";
--- loginPage.js
+++ loginPage.js
@@ -1,18 +1,46 @@
-class User
+class UserOwner
 {
-    constructor(username, password) {
+    constructor(username, password, name, address, email, contact) {
+        //Login info
         this.username = username;
         this.password = password;
+        //Profile info
+        this.name = name;
+        this.address = address;
+        this.email = email;
+        this.contact = contact;
     }
 }
 
-//test user data
-const users = [
-    new User('user1', 'password1'),
-    new User('user2', 'password2'),
-    new User('user3', 'password3'),
-]
+class UserWorker
+{
+    constructor(username, password, name, address, email, contact) {
+        //Login info
+        this.username = username;
+        this.password = password;
+        //Profile info
+        this.name = name;
+        this.address = address;
+        this.email = email;
+        this.contact = contact;
+    }
+}
 
+function intializeArray() {
+    const ownerUsersJSON = localStorage.getItem('ownerUsers');
+    const workerUsersJSON = localStorage.getItem('workerUsers');
+    return {
+        ownerUsers: ownerUsersJSON ? JSON.parse(ownerUsersJSON) : [],
+        workerUsers: workerUsersJSON ? JSON.parse(workerUsersJSON) : [],
+    };
+}
+
+const {ownerUsers, workerUsers} = intializeArray();
+
+function saveArray() {
+    localStorage.setItem('ownerUsers', JSON.stringify(ownerUsers));
+    localStorage.setItem('workerUsers', JSON.stringify(workerUsers));
+}
 
 //Login function for co-worker
     document.getElementById('coworker').addEventListener('submit', function(event)
@@ -22,7 +50,7 @@ const users = [
         const usernameInput = document.getElementById('usernameWorker').value;
         const passwordInput = document.getElementById('passwordWorker').value;
 
-        const foundUser = users.find(user => user.username === usernameInput && user.password === passwordInput);
+        const foundUser = workerUsers.find(UserWorker => UserWorker.username === usernameInput && UserWorker.password === passwordInput);
 
         if(foundUser) {
             checkWorker.innerHTML = "Login Successful!";
@@ -46,7 +74,7 @@ const users = [
         const usernameInput = document.getElementById('usernameOwner').value;
         const passwordInput = document.getElementById('passwordOwner').value;
 
-        const foundUser = users.find(user => user.username === usernameInput && user.password === passwordInput);
+        const foundUser = ownerUsers.find(UserOwner => UserOwner.username === usernameInput && UserOwner.password === passwordInput);
 
         if(foundUser) {
             checkOwner.innerHTML = "Login Successful!";
@@ -60,3 +88,69 @@ const users = [
             checkOwner.style.color = "red";
         }
     });
+
+    //Create account
+
+    function checkUser() {
+        var workerType = document.getElementById("worker");
+        var ownerType = document.getElementById("owner");
+        var verfi = document.getElementById("verfication")
+    
+        if(workerType.checked === true)
+        {
+            const newUser = createUserWorker();
+            workerUsers.push(newUser);
+            saveArray();
+            verfi.innerHTML="New account created successfully";
+            verfi.style.color="green";
+        }
+        else if(ownerType.checked === true)
+        {
+            const newUser = createUserOwner();
+            ownerUsers.push(newUser);
+            saveArray();
+            verfi.innerHTML="New account created successfully";
+            verfi.style.color="green";
+        }
+        else{
+            console.log("Not all information is inputted");
+        }
+    }
+    
+    function createUserOwner() 
+    {
+        const username = document.getElementById('username').value;
+        const password = document.getElementById('password').value;
+        const name = document.getElementById('name').value;
+        const address = document.getElementById('address').value;
+        const email = document.getElementById('email').value;
+        const contact = document.getElementById('contact').value;
+    
+        const newUserOwner = new UserOwner(username, password, name, address, email, contact);
+        
+        console.log("User created successfully");
+    
+        console.log(ownerUsers);
+        console.log(workerUsers);
+        
+        return newUserOwner;
+    }
+    
+    function createUserWorker() 
+    {
+        const username = document.getElementById('username').value;
+        const password = document.getElementById('password').value;
+        const name = document.getElementById('name').value;
+        const address = document.getElementById('address').value;
+        const email = document.getElementById('email').value;
+        const contact = document.getElementById('contact').value;
+    
+        const newUserWorker = new UserWorker(username, password, name, address, email, contact);
+        
+        console.log("User created successfully");
+    
+        console.log(ownerUsers);
+        console.log(workerUsers);
+        
+        return newUserWorker;
+    }
